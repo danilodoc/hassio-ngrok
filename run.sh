6a01a2563a6ab56b7c6dfd6f6b9dfbedb93b5dd5
Dylan <dylan.hasler@gmail.com>
2019-12-01 22:15:31 -0500

    Switch to bashio

--- run.sh
+++ run.sh
@@ -7,20 +7,19 @@ if [[ -f /share/ngrok.yml ]]; then
   cp $configFile /ngrok-config/ngrok.yml
 else
 
-  CONFIG_PATH=/data/options.json
-  auth_token=$(jq --raw-output ".auth_token" $CONFIG_PATH)
-  region=$(jq --raw-output ".region" $CONFIG_PATH)
-  port=$(jq --raw-output ".port" $CONFIG_PATH)
-  inspect=$(jq --raw-output ".inspect" $CONFIG_PATH)
-  subdomain=$(jq --raw-output ".subdomain" $CONFIG_PATH)
-  hostname=$(jq --raw-output ".hostname" $CONFIG_PATH)
-  use_tls=$(jq --raw-output ".use_tls" $CONFIG_PATH)
+  auth_token=$(bashio::config 'auth_token')
+  region=$(bashio::config 'region')
+  port=$(bashio::config 'port')
+  inspect=$(bashio::config 'inspect')
+  subdomain=$(bashio::config 'subdomain')
+  hostname=$(bashio::config 'hostname')
+  use_tls=$(bashio::config 'use_tls')
 
   echo "web_addr: 0.0.0.0:4040" > /ngrok-config/ngrok.yml
-  if [ "$auth_token" != "" ]; then
+  if [ -n "$auth_token" ]; then
     echo "authtoken: $auth_token" >> /ngrok-config/ngrok.yml
   fi
-  if [ "$region" != "" ]; then
+  if [ -n "$region" ]; then
     echo "region: $region" >> /ngrok-config/ngrok.yml
   else
     echo "No region defined, default region is US."
@@ -31,22 +30,22 @@ else
     echo "    proto: tls" >> /ngrok-config/ngrok.yml
   else 
     echo "    proto: http" >> /ngrok-config/ngrok.yml
-    if [ $inspect != "" ]; then
+    if [ -n $inspect ]; then
       echo "    inspect: $inspect" >> /ngrok-config/ngrok.yml
     fi
   fi
-  if [ "$port" != "" ]; then
+  if [ -n "$port" ]; then
     echo "    172.30.32.2:$port" >> /ngrok-config/ngrok.yml
   else
     echo "You must specify a port!"
     exit 1
   fi
-  if [ $hostname != "" ]; then
+  if [ -n $hostname ]; then
     echo "    hostname: $hostname" >> /ngrok-config/ngrok.yml
-  elif [ $subdomain != "" ]; then
+  elif [ -n $subdomain ]; then
     echo "    subdomain: $subdomain" >> /ngrok-config/ngrok.yml
   fi
-  cat /ngrok-config/ngrok.yml
+
   echo "Starting ngrok"
 fi
 
