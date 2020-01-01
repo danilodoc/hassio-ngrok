bfdf1ab953956a6ff7e7f4e34b120476388de850
Dylan <dylan@hasler.me>
2019-12-31 22:36:04 -0500

    Declare variables

--- ngrok/run.sh
+++ ngrok/run.sh
@@ -11,49 +11,60 @@ else
   echo "No region defined, default region is US."
 fi
 echo "tunnels:" >> /ngrok-config/ngrok.yml
-id=0
-for tunnel in $(bashio::config "tunnels|keys"); do
-  echo "  tunnel-$id" >> /ngrok-config/ngrok.yml
-  if [[ $(bashio::config "tunnels[${tunnel}].proto") != "null" ]]; then
-    echo "    proto: $(bashio::config 'tunnels[${tunnel}].proto')" >> /ngrok-config/ngrok.yml
+for id in $(bashio::config "tunnels|keys"); do
+  echo "  tunnel-$id:" >> /ngrok-config/ngrok.yml
+  proto=$(bashio::config "tunnels[${id}].proto")
+  if [[ $proto != "null" ]]; then
+    echo "    proto: $proto" >> /ngrok-config/ngrok.yml
   fi
-  if [[ $(bashio::config "tunnels[${tunnel}].addr") != "null" ]]; then
-    echo "    addr: $(bashio::config 'tunnels[${tunnel}].addr')" >> /ngrok-config/ngrok.yml
+  addr=$(bashio::config "tunnels[${id}].addr")
+  if [[ $addr != "null" ]]; then
+    echo "    addr: $addr" >> /ngrok-config/ngrok.yml
   fi
-  if [[ $(bashio::config "tunnels[${tunnel}].inspect") != "null" ]]; then
-    echo "    inspect: $(bashio::config 'tunnels[${tunnel}].inspect')" >> /ngrok-config/ngrok.yml
+  inspect=$(bashio::config "tunnels[${id}].inspect")
+  if [[ $inspect != "null" ]]; then
+    echo "    inspect: $inspect" >> /ngrok-config/ngrok.yml
   fi
-  if [[ $(bashio::config "tunnels[${tunnel}].auth") != "null" ]]; then
-    echo "    auth: $(bashio::config 'tunnels[${tunnel}].auth')" >> /ngrok-config/ngrok.yml
+  auth=$(bashio::config "tunnels[${id}].auth")
+  if [[ $auth != "null" ]]; then
+    echo "    auth: $auth" >> /ngrok-config/ngrok.yml
   fi
-  if [[ $(bashio::config "tunnels[${tunnel}].host_header") != "null" ]]; then
-    echo "    host_header: $(bashio::config 'tunnels[${tunnel}].host_header')" >> /ngrok-config/ngrok.yml
+  host_header=$(bashio::config "tunnels[${id}].host_header")
+  if [[ $host_header != "null" ]]; then
+    echo "    host_header: $host_header" >> /ngrok-config/ngrok.yml
   fi
-  if [[ $(bashio::config "tunnels[${tunnel}].bind_tls") != "null" ]]; then
-    echo "    bind_tls: $(bashio::config 'tunnels[${tunnel}].bind_tls')" >> /ngrok-config/ngrok.yml
+  bind_tls=$(bashio::config "tunnels[${id}].bind_tls")
+  if [[ $bind_tls != "null" ]]; then
+    echo "    bind_tls: $bind_tls" >> /ngrok-config/ngrok.yml
   fi
-  if [[ $(bashio::config "tunnels[${tunnel}].subdomain") != "null" ]]; then
-    echo "    subdomain: $(bashio::config 'tunnels[${tunnel}].subdomain')" >> /ngrok-config/ngrok.yml
+  subdomain=$(bashio::config "tunnels[${id}].subdomain")
+  if [[ $subdomain != "null" ]]; then
+    echo "    subdomain: $subdomain" >> /ngrok-config/ngrok.yml
   fi
-  if [[ $(bashio::config "tunnels[${tunnel}].hostname") != "null" ]]; then
-    echo "    hostname: $(bashio::config 'tunnels[${tunnel}].hostname')" >> /ngrok-config/ngrok.yml
+  hostname=$(bashio::config "tunnels[${id}].hostname")
+  if [[ $hostname != "null" ]]; then
+    echo "    hostname: $hostname" >> /ngrok-config/ngrok.yml
   fi
-  if [[ $(bashio::config "tunnels[${tunnel}].crt") != "null" ]]; then
-    echo "    crt: $(bashio::config 'tunnels[${tunnel}].crt')" >> /ngrok-config/ngrok.yml
+  crt=$(bashio::config "tunnels[${id}].crt")
+  if [[ $crt != "null" ]]; then
+    echo "    crt: $crt" >> /ngrok-config/ngrok.yml
   fi
-  if [[ $(bashio::config "tunnels[${tunnel}].key") != "null" ]]; then
-    echo "    key: $(bashio::config 'tunnels[${tunnel}].key')" >> /ngrok-config/ngrok.yml
+  key=$(bashio::config "tunnels[${id}].key")
+  if [[ $key != "null" ]]; then
+    echo "    key: $key" >> /ngrok-config/ngrok.yml
   fi
-  if [[ $(bashio::config "tunnels[${tunnel}].client_cas") != "null" ]]; then
-    echo "    client_cas: $(bashio::config 'tunnels[${tunnel}].client_cas')" >> /ngrok-config/ngrok.yml
+  client_cas=$(bashio::config "tunnels[${id}].client_cas")
+  if [[ $client_cas != "null" ]]; then
+    echo "    client_cas: $client_cas" >> /ngrok-config/ngrok.yml
   fi
-  if [[ $(bashio::config "tunnels[${tunnel}].remote_addr") != "null" ]]; then
-    echo "    remote_addr: $(bashio::config 'tunnels[${tunnel}].remote_addr')" >> /ngrok-config/ngrok.yml
+  remote_addr=$(bashio::config "tunnels[${id}].remote_addr")
+  if [[ $remote_addr != "null" ]]; then
+    echo "    remote_addr: $remote_addr" >> /ngrok-config/ngrok.yml
   fi
-  if [[ $(bashio::config "tunnels[${tunnel}].metadata") != "null" ]]; then
-    echo "    metadata: $(bashio::config 'tunnels[${tunnel}].metadata')" >> /ngrok-config/ngrok.yml
+  metadata=$(bashio::config "tunnels[${id}].metadata")
+  if [[ $metadata != "null" ]]; then
+    echo "    metadata: $metadata" >> /ngrok-config/ngrok.yml
   fi
-  $id+1
 done
 echo "Starting ngrok..."
 ngrok start --config /ngrok-config/ngrok.yml --all
