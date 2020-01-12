6bf2f7e8adda38ab2095cd095a166d94d164cd8c
Dylan Hasler <dylan@hasler.me>
2020-01-12 15:02:05 -0500

    remove local port as test

--- ngrok/rootfs/etc/cont-init.d/nginx.sh
+++ ngrok/rootfs/etc/cont-init.d/nginx.sh
@@ -5,22 +5,22 @@ declare ingress_interface
 declare ingress_port
 declare keyfile
 
-port=$(bashio::addon.port 4040)
-if bashio::var.has_value "${port}"; then
-    bashio::config.require.ssl
+# port=$(bashio::addon.port 4040)
+# if bashio::var.has_value "${port}"; then
+#     bashio::config.require.ssl
 
-    if bashio::config.true 'ssl'; then
-        certfile=$(bashio::config 'certfile')
-        keyfile=$(bashio::config 'keyfile')
+#     if bashio::config.true 'ssl'; then
+#         certfile=$(bashio::config 'certfile')
+#         keyfile=$(bashio::config 'keyfile')
 
-        mv /etc/nginx/servers/direct-ssl.disabled /etc/nginx/servers/direct.conf
-        sed -i "s#%%certfile%%#${certfile}#g" /etc/nginx/servers/direct.conf
-        sed -i "s#%%keyfile%%#${keyfile}#g" /etc/nginx/servers/direct.conf
+#         mv /etc/nginx/servers/direct-ssl.disabled /etc/nginx/servers/direct.conf
+#         sed -i "s#%%certfile%%#${certfile}#g" /etc/nginx/servers/direct.conf
+#         sed -i "s#%%keyfile%%#${keyfile}#g" /etc/nginx/servers/direct.conf
 
-    else
-        mv /etc/nginx/servers/direct.disabled /etc/nginx/servers/direct.conf
-    fi
-fi
+#     else
+#         mv /etc/nginx/servers/direct.disabled /etc/nginx/servers/direct.conf
+#     fi
+# fi
 
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
